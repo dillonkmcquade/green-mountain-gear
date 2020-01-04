d7a6bde826b3c7be00f82a724db210dee509fa00
dillonkmcquade <dillonkmcquade@gmail.com>
2020-01-04 18:30:00 -0500

    update package.json

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 import CollectionPreview from "../preview-collection/collection-preview.component";
-import "./collection-overview.styles.scss";
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import { createStructuredSelector } from "reselect";
+import { CollectionsOverviewContainer } from "./collection-overview.styles.jsx";
 
 const CollectionOverview = ({ collections }) => {
   return (
-    <div className="collections-overview">
+    <CollectionsOverviewContainer>
       {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview
-          key={id}
-          {...otherCollectionProps}
-          
-        />
+        <CollectionPreview key={id} {...otherCollectionProps} />
       ))}
-    </div>
+    </CollectionsOverviewContainer>
   );
 };
 
