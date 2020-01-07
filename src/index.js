955854321ba6d5f792cfb46e1a2000be1d9baa48
dillonkmcquade <dillonkmcquade@gmail.com>
2020-01-07 14:31:22 -0500

    added service worker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
+import * as serviceWorker from './serviceWorker';
 import "./index.css";
 import App from "./App";
 import "tachyons";
@@ -18,3 +19,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
