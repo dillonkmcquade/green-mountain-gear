ffd785341bfe5c5344227ae8f21a656a4dbeea3b
dillonkmcquade <dillonkmcquade@gmail.com>
2020-01-20 18:06:23 -0500

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import App from "./App";
 import "tachyons";
